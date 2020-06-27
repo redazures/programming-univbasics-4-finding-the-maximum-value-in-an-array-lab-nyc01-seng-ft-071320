941020e293b73a6c11fcba47b13404e4a39b64c8
james wu <jwu_nyc@yahoo.com>
2020-06-27 02:01:47 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -4,7 +4,7 @@ def find_max_value(array)
   counter=0
   while counter<array.length
     if array[counter]>win
-      win=counter
+      win=array[counter]
     end
     counter+=1
     puts counter
