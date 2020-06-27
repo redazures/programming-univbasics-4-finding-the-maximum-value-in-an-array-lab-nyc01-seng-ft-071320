6d2c42b837081f99699d2a7028629f3951b61207
james wu <jwu_nyc@yahoo.com>
2020-06-27 02:05:08 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,14 +1,10 @@
 def find_max_value(array)
   # Add your solution here
   win=""
-  counter=0
-  while counter<array.length
-    if array[counter]>win
-      win=array[counter]
-    end
-    counter+=1
-    puts counter
+  array.each do |value|
+    win=value if win<value
   end
+
   win
 end
 
