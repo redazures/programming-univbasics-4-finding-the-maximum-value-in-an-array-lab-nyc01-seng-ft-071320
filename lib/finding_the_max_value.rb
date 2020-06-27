881a21d949e55c80dc96ed8f952f0aa9e998633d
james wu <jwu_nyc@yahoo.com>
2020-06-27 02:06:40 +0000

    Done.

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,13 @@
 def find_max_value(array)
   # Add your solution here
+  win=-100
+  array.each do |value|
+    win=value if win<value
+  end
+
+  win
 end
+
+m=[1,100,99,951]
+
+puts find_max_value(m)
